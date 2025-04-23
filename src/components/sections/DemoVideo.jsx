f7966f406d3a98feecce764ba7f2751287e950dc
jonathanyangg <jonathan.yang.x@gmail.com>
2025-04-22 21:01:19 -0400

    energy video

--- src/components/sections/DemoVideo.jsx
+++ src/components/sections/DemoVideo.jsx
@@ -2,7 +2,7 @@ import YouTubePlayer from '../media/YouTubePlayer';
 
 export default function DemoVideo() {
   // Video ID extracted from the URL: https://www.youtube.com/watch?v=m0wAlnTPHRE
-  const videoId = 'm0wAlnTPHRE';
+  const videoId = 'O4pfKNBziGI';
   
   return (
     <section className="py-16 bg-[#0a0a0a] px-4">
